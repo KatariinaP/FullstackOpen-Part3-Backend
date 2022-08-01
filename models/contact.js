de51d4c881e083f8ad6679cb1b4117510513694e
Katariina Piippanen <kpiippanen@gmail.com>
2022-08-01 17:43:10 +0200

    3.20

--- models/contact.js
+++ models/contact.js
@@ -16,10 +16,19 @@ mongoose.connect(url)
     name: {
       type: String,
       minlength: 3,
-      required: true
+      required: true,
+      unique: true
     },
-    number: String,
-  })
+    number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d+/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  }})
 
 contactSchema.set('toJSON', {
     transform: (document, returnedObject) => {
